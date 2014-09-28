93c16a7fdaba57814bd7ef8d23c72bc365b014a4
Preston Blaylock <Preston@Prestons-MacBook-Pro.local>
2014-09-27 22:38:00 -0600

    Home Page

---
+++ modules/head.php
@@ -0,0 +1,16 @@
+<head>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="/js/jquery-ui/jquery-ui.min.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script src="/js/jquery-ui/jquery-ui.min.js"></script>
+  <script>
+  $(function() {
+    $( "#tabs" ).tabs();
+  });
+  </script>
+</head>
---
+++ modules/nav.php
@@ -0,0 +1,4 @@
+<ul>
+  <li><a href="#tabs-1">About Me</a></li>
+  <li><a href="#tabs-2">Assignments</a></li>
+</ul>
