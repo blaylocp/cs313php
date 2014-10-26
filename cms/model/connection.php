8e9379de9379de67c2e23316979b8ab430eefb3b
Preston Blaylock <Preston@Prestons.local>
2014-10-26 01:52:59 -0600

    connection fix

--- cms/model/connection.php
+++ cms/model/connection.php
@@ -1,19 +1,19 @@
 <?php
   //locally
-   $host = 'localhost';
-   $username = 'root';
-   $password = '';
-   $database = 'cms';
+   //$host = 'localhost';
+   //$username = 'root';
+   //$password = '';
+   //$database = 'cms';
 
-  //$db_host = $_ENV['OPENSHIFT_MYSQL_DB_HOST'];
-  //$db_user = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
-  //$db_pass = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
-  //$db_name = $_ENV['OPENSHIFT_APP_NAME'];
-  //$db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
+     $db_host = $_ENV['OPENSHIFT_MYSQL_DB_HOST'];
+     $db_user = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
+     $db_pass = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
+     $db_name = $_ENV['OPENSHIFT_APP_NAME'];
+     $db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
 
-  //$connection = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
+     $connection = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
-   $connection = new mysqli($host, $username, $password, $database);
+   //$connection = new mysqli($host, $username, $password, $database);
 
   if(mysqli_connect_error()){
     echo 'bad connection';
